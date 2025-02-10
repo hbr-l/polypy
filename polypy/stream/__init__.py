2554d65fb1fc8e565cd4c75bedc2b79fb001287b
LHuebser <louis.huebser@icloud.com>
2025-02-10 02:44:36 +0100

    Renaming to BufferTrheadSettings

--- polypy/stream/__init__.py
+++ polypy/stream/__init__.py
@@ -1,6 +1,6 @@
 from polypy.stream.market import STATUS_ORDERBOOK, CheckHashParams, MarketStream
 from polypy.stream.user import (
-    BufferSettings,
+    BufferThreadSettings,
     MarketAssetsInfo,
     TupleManager,
     UserStream,
