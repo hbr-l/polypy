591adc31f68fb7e60a4e6b1cac29e70b03123553
LHuebser <louis.huebser@icloud.com>
2025-10-07 23:36:48 +0200

    Improve test stability

--- tests/ipc/test_shared_rlock.py
+++ tests/ipc/test_shared_rlock.py
@@ -7,13 +7,13 @@ from polypy.ipc.lock import SharedProcRLock
 
 
 def test_rlock_acquire_release():
-    rlock = SharedProcRLock("test", True)
+    rlock = SharedProcRLock("test_rlock_acquire_release", True)
     assert rlock.acquire()
     assert rlock.release() is None
 
 
 def test_rlock_reentrance():
-    rlock = SharedProcRLock("test", True)
+    rlock = SharedProcRLock("test_rlock_reentrance", True)
     assert rlock.acquire()
     assert rlock.acquire()
     assert rlock.acquire()
@@ -22,30 +22,30 @@ def test_rlock_reentrance():
     assert rlock.release() is None
 
 
-def worker():
-    rlock = SharedProcRLock("test", False)
+def worker(name: str):
+    rlock = SharedProcRLock(name, False)
     rlock.acquire()
-    time.sleep(5)
+    time.sleep(8)
     rlock.release()
 
 
 def test_rlock_blocks():
-    rlock = SharedProcRLock("test", True)
+    rlock = SharedProcRLock("test_rlock_blocks", True)
 
-    proc = multiprocessing.Process(target=worker)
+    proc = multiprocessing.Process(target=worker, args=("test_rlock_blocks",))
     proc.start()
-    time.sleep(2)
+    time.sleep(3)
 
     assert rlock.acquire(False) is False
     time.sleep(3)
-    proc.join(5)
+    proc.join(8)
 
     assert rlock.acquire()
     assert rlock.release() is None
 
 
-def worker_context_reentrance():
-    rlock = SharedProcRLock("test", False)
+def worker_context_reentrance(name: str):
+    rlock = SharedProcRLock(name, False)
     with rlock:
         time.sleep(3)
         rlock.acquire()
@@ -54,33 +54,38 @@ def worker_context_reentrance():
 
 
 def test_rlock_blocks_context_with_reentrance():
-    rlock = SharedProcRLock("test", True)
+    rlock = SharedProcRLock("test_rlock_blocks_context_with_reentrance", True)
 
-    proc = multiprocessing.Process(target=worker_context_reentrance)
+    proc = multiprocessing.Process(
+        target=worker_context_reentrance,
+        args=("test_rlock_blocks_context_with_reentrance",),
+    )
     proc.start()
-    time.sleep(2)
+    time.sleep(3)
 
     assert rlock.acquire(True, 2) is False
     time.sleep(4)
-    proc.join(6)
+    proc.join(8)
 
     assert rlock.acquire()
     assert rlock.release() is None
 
 
 def test_rlock_release_without_acquire():
-    rlock = SharedProcRLock("test", True)
+    rlock = SharedProcRLock("test_rlock_release_without_acquire", True)
 
     with pytest.raises(RuntimeError):
         rlock.release()
 
 
 def test_rlock_release_wrong_process():
-    rlock = SharedProcRLock("test", True)
+    rlock = SharedProcRLock("test_rlock_release_wrong_process", True)
 
-    proc = multiprocessing.Process(target=worker)
+    proc = multiprocessing.Process(
+        target=worker, args=("test_rlock_release_wrong_process",)
+    )
     proc.start()
-    time.sleep(2)
+    time.sleep(3)
 
     with pytest.raises(RuntimeError):
         rlock.release()
