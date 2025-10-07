2d56201c5cbb67801e506503f6d5d88e28283ba5
LHuebser <louis.huebser@icloud.com>
2025-10-07 23:37:40 +0200

    Rename dict_to_book_struct

--- polypy/__init__.py
+++ polypy/__init__.py
@@ -5,7 +5,7 @@ from polypy.book.order_book import (
     calculate_marketable_price,
 )
 from polypy.book.parsing import (
-    dict_to_market_event_struct,
+    dict_to_book_struct,
     guess_tick_size,
     message_to_orderbook,
 )
